25981b6599e82ba17175e8af3cfec9a16665bd32
Steven Santiago-Perez <santiagoperezsteven@gmail.com>
2023-12-11 23:58:41 -0500

    gpcomp

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { render } from 'react-dom'
 // 👉 App contains a more sophisticated form we'll flesh out later
 import App from './components/App'
@@ -11,8 +11,49 @@ const petsList = [
 ]
 
 function SimpleForm() {
-  return <div>Ready to start GP!</div>
-}
+  const [pets, setPets] = useState(petsList);
+  const [formValues, setFormValues] = useState({ petName: "" , petType: "" })
+
+  const change = (evt) => {
+    const {name, value } = evt.target;
+
+    setFormValues({...formValues, [name]: value})
+  }
+
+  const submit = (evt) => {
+    evt.preventDefault();
+    setPets(pets.concat({petName: formValues.petName, petType: formValues.petType}));
+  }
+
+
+
+  return (
+    <div className='container'>
+      <h1>Simple Form App</h1>
+      {petsList.map((pet, idx) => {
+        return (
+          <div key={idx}>
+          {pet.petName} is a {pet.petType}
+          </div>
+        )
+      })}
+      <form onSubmit={submit} >
+        <input
+          name="petName"
+          type="text"
+          value={formValues.petName}
+          onChange={change}
+        />
+        <input
+          name="petType"
+          type="text"
+          value={formValues.petType}
+          onChange={change}
+        />
+        <input type='submit' value="Create a Pet!" />
+      </form>
+    </div>
+)}
 
 render(
   <>
